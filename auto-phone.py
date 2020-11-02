f495b03935854c229e1dfe4966c126221fda207d
achillesg007 <voidmainx@gmail.com>
2020-11-02 18:33:49 -0500

    initial page commit

--- auto-phone.py
+++ auto-phone.py
@@ -1,13 +1,13 @@
 import os
 from twilio.rest import Client
 
-account_sid = "AC46e8b3518033f0a1852f70556c12e732"
-auth_token = "20a934aebd9cd4cdc7864d247179f495"
+account_sid = "your-sid-here"
+auth_token = "your-auth-token-here"
 
 client = Client(account_sid, auth_token)
 
 client.messages.create(
-    to="+13214327181",
-    from_ = "+16892202046",
-    body="Your message just got sent.",
+    to="receiver",
+    from_ = "twilio-number",
+    body="I have the high ground.",
 )
